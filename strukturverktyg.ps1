1426af3c0266dfaf33407ab68f67fe21ce97552a
unknown <daud357@outlook.com>
2025-05-16 09:03:12 +0200

    skapade variabler i funktionen

--- strukturverktyg.ps1
+++ strukturverktyg.ps1
@@ -2,3 +2,32 @@
 # YH24
 
 $namn = Read-Host "Ange namn för system/miljö"
+
+function FunctionName {
+    param ($namn)
+    
+    $datum = Get-Date -Format "yyyy-MM-dd"
+    $tid = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $huvudmapp = ".\$namn"
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
